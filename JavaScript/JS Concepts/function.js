23024567ade6341ae9df84b60c50d4afb704df09
Pushkar Raj Soni <pushkarrajsoni40@gmail.com>
2022-12-16 22:23:21 +0530

    comment added

--- JavaScript/JS Concepts/function.js
+++ JavaScript/JS Concepts/function.js
@@ -13,10 +13,11 @@
 // console.log("rVal from fn ", rVal);
 
 function fnwithPar(a, b) {
-    console.log(a, " ", b);
-    return "return value";
+    console.log(a + b); //console is used for to check the values and print the values just for to show that what it is holding
+    return a+b; //it is stoping the execution and returning the value 
 
 }
-fnwithPar(10, 20);
-let rVal = fnwithPar("hello", 20);
-console.log(rVal);
+// fnwithPar(10, 20);
+let rVal = fnwithPar(30 , 20);
+console.log("gg",rVal);
+// fnwithPar(20, 75);
