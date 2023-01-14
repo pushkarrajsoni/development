11d69b39708f31ccbcf6ef62680e8a7cf1920d98
Pushkar Raj Soni <pushkarrajsoni40@gmail.com>
2023-01-15 01:06:05 +0530

    commit

---
+++ JavaScript/JS Concepts/recordCollection.js
@@ -0,0 +1,27 @@
+let recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  function getValues(recordCollection, id, prop) {
+    console.log(recordCollection[id][prop]); //prop -> property
+    
+  }
+  
+  getValues(recordCollection, 5439, "albumTitle");
+  getValues(recordCollection,2468, "tracks");
